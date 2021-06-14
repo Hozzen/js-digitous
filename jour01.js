2d3681e529b6818696c9b7dd3a8419384a93499f
Kemayou Kévin <kemayou.ke@gmail.com>
2021-06-14 13:51:35 +0200

    Exercice 1 à 8

--- jour01.js
+++ jour01.js
@@ -1,4 +1,58 @@
-// 01 - Hello world
+/* // 01 - Hello world
 
-console.log("Hello world!");
+console.log("Hello world!"); */
 
+// 02 - String
+
+/* var test = "My name is Kévin"
+
+console.log(test)
+
+ */
+
+// 03 - Concatenation
+
+/* var name = "Kévin"
+
+console.log("Nice to meet you " + name) */
+
+// console.log(`Nice to meet you ${name}`)
+
+/* 04 - String length
+
+var testLength = "I'm very long !"
+
+console.log(testLength.length)
+ */
+
+// 05 - Replace
+
+/* var food = "croissant is meh"
+
+console.log(food.replace("meh", "so good")) */
+
+// 06 - Up and down
+
+/* var basic = "This is Cool"
+var basicUp = "BASIC"
+var basicDown = "basic"
+
+console.log(basic, basicUp, basicDown) */
+
+
+// 07 - Split
+
+/* var word = "bannana"
+var letters = word.split("");
+
+console.log(letters) */
+
+// 08 - template
+
+var age = 22
+
+var template = "i'm" + ` ${age} ` + "years old"
+
+var template = `i'm ${age} years old`
+
+console.log(template)

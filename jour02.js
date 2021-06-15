2ac2e2487750a958078e8f9d66dc80332dc333d9
Kemayou Kévin <kemayou.ke@gmail.com>
2021-06-15 14:05:28 +0200

    jour02 initial commit

---
+++ jour02.js
@@ -0,0 +1,123 @@
+/* // 01 - Number
+
+let interger = 102
+let float = 13.9
+
+console.log(interger, float)
+
+
+// 02 - Conevert 
+
+let basic = 34
+let stringfied = basic.toString()
+
+console.log(basic, stringfied)
+
+// 03 - Round 
+
+let num = 1.5
+let rounded = Math.round(num)
+
+console.log(rounded)
+ */
+
+// 04 - Arithmétique
+
+/* let test = 12
+let bis = 5
+
+console.log(test + bis) */
+
+// 05 - Comparaison
+/* 
+let test = 143
+let bis = 219
+
+console.log(test > bis) */
+
+// 06 - Condition
+
+/* let limit = 50
+let score = 64
+
+if (score >= limit) {
+    console.log("Ok good !")
+} else {
+    console.log("Oh noo...")
+}
+
+// 07 - Condition II
+
+let password = "azerty"
+
+if (password.length > 5) {
+    console.log("The password is secure")
+} else {
+    console.log("Passeword should be more than 5 characters")
+} */
+
+// 08 - Condition III
+
+/* if (password.length > 5 && score >= limit) {
+    console.log("Everything is good")
+} else if (password.length > 5 || score >= limit) {
+    console.log("Something is good")
+} else {
+    console.log("Nothing is good")
+} */
+
+// Bonus I
+
+/* let random = Math.floor(Math.random() * 6 +1)
+
+console.log(random)
+
+if (random === 6) {
+    console.log("Yes i win!")
+} else {
+    console.log("So close...")
+} */
+
+// Bonus II
+
+let month = "January"
+
+switch(month) {
+    case "January":
+        console.log("Winter");
+    break;
+    case "February":
+        console.log("Winter");
+    break;
+    case "March":
+        console.log("Spring");
+    break;
+    case "April":
+        console.log("Spring");
+    break;
+    case "May":
+        console.log("Spring");
+    break;
+    case "June":
+        console.log("Summer");
+    break;
+    case "July":
+        console.log("Summer");
+    break;
+    case "August":
+        console.log("Summer");
+    break;
+    case "September":
+        console.log("Autumn");
+    break;
+    case "October":
+        console.log("Autumn");
+    break;
+    case "November":
+        console.log("Autumn");
+    break;
+    case "December":
+        console.log("Winter");
+    break;
+
+}

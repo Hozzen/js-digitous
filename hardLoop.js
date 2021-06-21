a2d86796ff4f3acb4f65bf10dcbc760a6b50f5d0
Kemayou Kévin <kemayou.ke@gmail.com>
2021-06-21 17:30:11 +0200

    Last commit of the day

--- hardLoop.js
+++ hardLoop.js
@@ -37,7 +37,7 @@ console.log(biggestNum) */
 
 // 03 - Unique
 
-let numbers = []
+/* let numbers = []
 min = 50
 max = 200
 for (let i = 0 ; i < 50 ; i++) {
@@ -55,4 +55,31 @@ while(x < numbers.length){
     }
     x++
 }
-console.log(result)
+console.log(result) */
+
+// 04 - Nested Loop 
+
+/* let words = ["hello", "goodbye", "yes", "no", "stop", "go go go"];
+
+let splitWords = []
+
+for (let i = 0; i < words.length ; i++) {
+    splitWords.push(words[i].split(""))
+
+}
+console.log(splitWords) */
+
+/* let oCount = 0
+let count = 0
+for(let i = 0 ; i < splitWords.length; i++){
+    if(splitWords[i] == "o"){
+        oCount++
+    }
+    count++
+}
+
+console.log(oCount)
+console.log(count) */
+
+
+

eecfbefe880d7500cf1dfa8ac6b94150b18cc413
Kemayou Kévin <kemayou.ke@gmail.com>
2021-06-21 15:29:40 +0200

    box box box box é_è exercice 4 done

---
+++ bouclesWhile.js
@@ -0,0 +1,55 @@
+// 01 - Comptons
+
+/* let min = 50
+let max = 200
+while(min <= max) {
+    if(min % 2 ===0){
+        console.log(min)
+    }
+    min++
+}
+ */
+// 02 - Try again
+
+/* let dice = null
+let count = 0
+
+while(dice !== 6) {
+    dice = Math.floor(Math.random() * 6 +1 )
+    count++
+}
+
+console.log(count) */
+
+// 03 - Course
+
+let ussainBolt = 0;
+
+let tysonGay = 0;
+
+while(ussainBolt <= 100 && tysonGay <= 100) {
+    random1 = Math.floor(Math.random() * 10 +1);
+    random2 = Math.floor(Math.random() * 10 +1);
+
+    ussainBolt += random1;
+    tysonGay += random2;
+
+}
+
+if (ussainBolt >= 100) {
+    console.log("Ussain BOLT EST VAINQUEUR!!!")
+} else if (tysonGay >= 100) {
+    console.log("Tyson GAY EST VAINQUEUR!!!")
+}
+
+// 04 - Des boites
+
+1st loop : console.log(box2) : 5
+           console.log(box1) : 2
+
+2nd loop : console.log(box2) : 5
+           console.log(box1) : -8
+
+
+// 05 - Encore des boites 
+

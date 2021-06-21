bb7b01492d83460511c726c9ce2ad0279cd49e0b
Kemayou Kévin <kemayou.ke@gmail.com>
2021-06-21 12:29:44 +0200

    boucleFor inital commit

---
+++ boucleFor.js
@@ -0,0 +1,46 @@
+/* // 01 - Sommes des carrés
+
+var result = 0
+
+for (let i = 5; i <= 10; i++) {
+    result += (i*i)
+}
+
+console.log(result)
+
+// 02 - Comptons
+
+var count = 0
+for (let i = 100; i <= 1000; i++) {
+    if (i % 7 === 0) {
+        count += 1
+    }
+}
+
+console.log(count) */
+
+// 03 - Chanceux
+
+/* let result = 0;
+
+for (let i = 0; i < 20; i++) {
+    let dice = Math.floor(Math.random() * 6 +1)
+
+    if (dice <= 5) {
+        result += dice;
+    }
+}
+
+console.log(result) */
+
+
+// 04 - Des boites
+console.log(box1+box2) : 10 (In the loop)
+console.log(box1) : 5
+console.log(box2) : 18
+console.log(i) : 13
+
+// 05 - Des setiob 
+
+
+

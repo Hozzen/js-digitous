1e803fa8fe8436e6ee86f40d7a52043f356e3f5a
Kemayou Kévin <kemayou.ke@gmail.com>
2021-06-29 17:20:27 +0200

    last commit of the day

--- jour08.js
+++ jour08.js
@@ -84,7 +84,7 @@ catchPokemon() */
 
 // Feels bad to be an idiot... Let's try again
 
-var axios = require("axios");
+/* var axios = require("axios");
 
 const catchPokemon = (id) => {
     axios
@@ -102,9 +102,87 @@ const catchPokemon = (id) => {
         })
 }
 
-catchPokemon(9)
+catchPokemon(9) */
+
 
 // Feels better to succeed.. guess i could've been faster tho, i hope i'll remember all i've done.
+
+
+// Bonus - Black Jack
+
+
+/* const { random } = require("colors")
+var prompt = require("prompt")
+prompt.start()
+
+var min = 16
+var max = 21
+
+
+// console.log(bank)
+// console.log(randomCard)
+
+
+const blackJack = () => {
+
+    var bankScore = 0;
+    var playerScore = 0;
+    var bankCard = Math.floor(Math.random() * (max - min +1 ) + min);
+    var randomCard = Math.floor(Math.random() * 10 +1);
+
+    playerScore += randomCard;
+
+    console.log(`Le joueur pioche un ${playerScore}`)
+    prompt.get({name: "choice" ,description: "Tapez 1 pour piocher 2 pour vous coucher"}, function(err, res) {
+
+        if (res.choice === 1) {
+            playerScore += randomCard;
+            console.log(`Le joueur pioche un ${randomCard}`);
+            console.log(`Le joueur est a ${playerScore}`);
+        } else if (res.choice === 2) {
+            bankScore += bankCard;
+            console.log(`Le croupier à ${bankScore}`)
+        } else {
+            console.log("Vous n'avez pas tapé 1 ni 2 !!")
+        }
+        if (err) {
+            return onErr(err);
+        }
+    });
+} */
+
+/* var prompt = require("prompt")
+prompt.start()
+
+const blackJack = () => {
+    var playerScore = 0;
+
+    for (let i = 0; i <= 21; i + playerScore){
+        var randomCard = Math.floor(Math.random() * 10 +1);
+        prompt.get({name: "choice" ,description: "Tapez a pour piocher b pour vous coucher"}, function(err, res){
+            if (res.choice === "a") {
+                playerScore += randomCard;
+                console.log(`Le joueur pioche un ${randomCard}`);
+                console.log(`Le joueur est a ${playerScore}`);
+            } else {
+                console.log("Bizarre bizarre")
+            }
+
+            if (err) {
+                return onErr(err)
+            }
+        })
+    }
+
+    if (playerScore < 21) {
+        console.log("Le joueur à bust")
+    }
+} */
+
+
+blackJack()
+
+
 
 
 
